36516bbcebbce0a554a899839f7bc515a10f0245
André Christoffer Andersen <andre@andersen.im>
2020-02-14 01:27:42 +0100

    Adding password.

--- home2telldus/app.py
+++ home2telldus/app.py
@@ -1,12 +1,13 @@
 import os
 from flask import Flask
+from flask import abort
 from flask import jsonify
 from flask import request
 
 from home2telldus.h2t import Home2TelldusClient
 
 app = Flask(__name__)
-app.config['DEBUG'] = is_debug = os.environ.get('ENVIRONMENT', 'production') == 'debug'
+app.config['DEBUG'] = is_debug = os.environ.get('APP_ENV', 'production') == 'debug'
 
 
 @app.route('/')
@@ -21,6 +22,12 @@ def api():
 
 @app.route('/api')
 def api():
+    actual_secret = os.environ.get('APP_SECRET')
+    claimed_secret = request.args.get('secret')
+
+    if actual_secret != claimed_secret:
+        abort(401)
+
     command = request.args.get('command')
     device_name = request.args.get('device_name')
 
