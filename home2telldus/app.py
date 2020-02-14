96dbbb089ba871dee097b6d043f00ca57d84685f
André Christoffer Andersen <andre@andersen.im>
2020-02-14 01:23:53 +0100

    Adding api endpoint.

--- home2telldus/app.py
+++ home2telldus/app.py
@@ -1,5 +1,9 @@
 import os
 from flask import Flask
+from flask import jsonify
+from flask import request
+
+from home2telldus.h2t import Home2TelldusClient
 
 app = Flask(__name__)
 app.config['DEBUG'] = is_debug = os.environ.get('ENVIRONMENT', 'production') == 'debug'
@@ -13,3 +17,14 @@ def main():
 @app.route('/api')
 def api():
     return 'api'
+
+
+@app.route('/api')
+def api():
+    command = request.args.get('command')
+    device_name = request.args.get('device_name')
+
+    with Home2TelldusClient() as client:
+        client.run(device_name, command)
+
+    return jsonify({'message': 'success'})
